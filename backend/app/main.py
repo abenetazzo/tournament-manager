874bd8863cbf0e4a899cb69b4481ba031d1ae793
Abenetazzo <antonio.benetazzo89@gmail.com>
2026-03-01 22:36:49 +0100

    Corretto errore CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI(title="Tournament Manager API")
 
+allowed_origins = [
+    "http://localhost:3000",
+    "https://tournament-manager-chi.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://tournament-manager-chi.vercel.app"
-        ],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
